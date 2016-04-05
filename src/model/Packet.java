864fe483c1e31c77d6eeab3cf01a7cfc9ca3eeaf
coen <coenneefjes@hotmail.com>
2016-04-05 12:59:28 +0100

    updated packet created datatable 'what a genius' - everyone

--- src/model/Packet.java
+++ src/model/Packet.java
@@ -5,6 +5,26 @@ package model;
  */
 public class Packet {
 
+    private int sourceAddress;
+    private int destinationAddress;
+    private DataTable data;
 
+    public Packet(int sourceAddress, int destinationAddress, DataTable data) {
+        this.sourceAddress = sourceAddress;
+        this.destinationAddress = destinationAddress;
+        this.data = data;
+    }
+
+    public int getSourceAddress() {
+        return sourceAddress;
+    }
+
+    public int getDestinationAddress() {
+        return destinationAddress;
+    }
+
+    public DataTable getData() {
+        return data;
+    }
 
 }
