d690c76b2546cd31dc67cea4b6e1c189f93ed129
ossyrial <rensieree@hotmail.com>
2016-04-05 15:47:02 +0200

    Added todos

--- src/model/Security.java
+++ src/model/Security.java
@@ -4,4 +4,15 @@ package model;
  * Created by Rens on 5-4-2016.
  */
 public class Security {
+
+    //What you want to do is encrypt every message.
+    //TODO: create way to handle incoming messages
+
+    //TODO: create way to share session keys
+
+    //TODO: create way to sign digitally
+
+
+
+
 }
