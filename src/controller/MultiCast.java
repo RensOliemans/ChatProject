f38a535dd1aaa1b40608f037fd2e3066468cbf51
birtebrunt <birte.brunt@hotmail.com>
2016-04-07 13:50:54 +0200

    TCP zo goed als af

--- src/controller/MultiCast.java
+++ src/controller/MultiCast.java
@@ -8,6 +8,7 @@ import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.net.UnknownHostException;
 import java.util.*;
+import model.TCP;
 
 import view.*;
 import controller.*;
@@ -26,6 +27,11 @@ public class MultiCast implements Runnable{
     InetAddress group;
     MulticastSocket s;
     TCP tcp;
+    TCPReceive tcpreceive;
+    Map<Byte, TCPReceive> receivers = new HashMap<>();
+    Map<Byte, TCP> senders = new HashMap<>();
+    public int computerNumber;
+
 
     public void setup() {
         try {
@@ -52,10 +58,40 @@ public class MultiCast implements Runnable{
 
     public void receive() {
         try {
+            TCP tcpr = null;
             byte[] buf = new byte[1000];
             DatagramPacket recv = new DatagramPacket(buf, buf.length);
             this.s.receive(recv);
-            tcp.handleMessage(recv);
+            byte[] data = recv.getData();
+            for (Map.Entry<Byte, TCP> e : senders.entrySet()){
+                if (e.getKey().equals(data[0])){
+                    tcpr = e.getValue();
+                }
+            }
+            if ((data[1] == 1 || data[1] == 2 || data[1] == 3 || data[1] == 4) && data[0] == data[1] && data.length == 2){
+                tcpreceive = new TCPReceive(data[1]);
+                receivers.put(data[1], tcpreceive);
+                tcpreceive.handleMessage(recv);
+            }
+            else if (tcpr == null){
+                for (Map.Entry<Byte, TCPReceive> e : receivers.entrySet()){
+                    if (e.getKey().equals(data[0])){
+                        tcpreceive = e.getValue();
+                    }
+                }
+                tcpreceive.handleMessage(recv);
+            }
+            else if ((data[1] == 0 && data.length == 2) || (data.length > tcp.HEADER+1 && !(tcp.HEADER == 1 && data[1] == 0 && data[2] == 0))){
+                for (Map.Entry<Byte, TCPReceive> e : receivers.entrySet()){
+                    if (e.getKey().equals(data[0])){
+                        tcpreceive = e.getValue();
+                    }
+                }
+                tcpreceive.handleMessage(recv);
+            }
+            else {
+                tcpr.handleMessage(recv);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -71,8 +107,19 @@ public class MultiCast implements Runnable{
         }
     }
 
-    public void send(String msg) {
-        tcp = new TCP();
+    public void send(String msg, int computernumber) {
+        tcp = new TCP(computernumber);
+        while (!tcp.getFirstReceived()){
+            byte[] first = new byte[2];
+            first[0] = (byte) computernumber;
+            first[1] = (byte) computernumber;
+            sendack(first);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         try {
             List<byte[]> splitmessages = tcp.splitMessages(msg);
             List<byte[]> message = tcp.addSendData(splitmessages);
@@ -91,8 +138,20 @@ public class MultiCast implements Runnable{
         while (tcp.getNotreceived()!=null){
             Map<byte[], byte[]> notreceived = tcp.getNotreceived();
             for (Map.Entry<byte[], byte[]> e : notreceived.entrySet()){
+                byte[] packet = new byte[e.getValue().length+1];
                 sendack(e.getValue());
             }
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        while (!tcp.getFinishReceived()){
+            byte[] finish = new byte[2];
+            finish[0] = (byte) computernumber;
+            finish[1] = (byte) 0;
+            sendack(finish);
             try {
                 Thread.sleep(100);
             } catch (InterruptedException e) {
