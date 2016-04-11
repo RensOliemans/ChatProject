7ebd55e7c1539ab5adc2c448f8824de355ebc05c
birtebrunt <birte.brunt@hotmail.com>
2016-04-11 13:06:22 +0200

    Multicast verbeterd

--- src/controller/MultiCast2.java
+++ src/controller/MultiCast2.java
@@ -328,7 +328,7 @@ public class MultiCast2 implements Runnable{
             //SYN 1 is reserved for the ACK of the FIN message
             List<byte[]> splitmessages = splitMessages(msg);
             for (byte[] packet : splitmessages) {
-                TextPacket toSend = new TextPacket(computerNumber, destination, seqint, new String(packet));
+                TextPacket toSend = new TextPacket(computerNumber, destination, seq, new String(packet));
                 System.out.println(seq[0] + "" + seq[1] + seq[2] + seq[3]);
                 DatagramPacket messagePacket = new DatagramPacket(toSend.getTextPacket(), toSend.getTextPacket().length, group, PORT);
                 this.s.send(messagePacket);
